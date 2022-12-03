492ed8726ba76d3ff9e4909146e874f484bfb432
Byzantine7 <119734720+Byzantine7@users.noreply.github.com>
2022-12-03 22:10:49 +0900

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -2,10 +2,10 @@ const setClock = document.querySelector("#clock");
 
 function time(){
     const date = new Date();
-    const hours = String(date.getHours()).padStart(2, "0");         //String()  .padStart(2,"") 스트링 함수의 구조() miss..
+    const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
     // const seconds = String(date.getSeconds()).padStart(2, "0");
-    setClock.innerText = `${hours}:${minutes}`;            //시간이 초마다 갱신되려면 time 함수에 포함되야...
+    setClock.innerText = `${hours}:${minutes}`;
 };
 
 time();
